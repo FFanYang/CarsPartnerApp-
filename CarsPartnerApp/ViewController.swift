5117230f21c62dcc958266acc06dcb7e80524fa5
Ivan Fan <47232921+FFanYang@users.noreply.github.com>
2020-11-02 00:09:38 +1300

    create newfiles

---
+++ CarsPartnerApp/ViewController.swift
@@ -0,0 +1,105 @@
+//
+//  ViewController.swift
+//  CarsPartnerApp
+//
+//  Created by Fan Yang on 9/10/20.
+//
+
+import UIKit
+import FirebaseAuth
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var drivelabel: UILabel!
+    
+    @IBOutlet weak var Riderlabel: UILabel!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var driverRiderSwitch: UISwitch!
+    
+    @IBOutlet weak var topButton: UIButton!
+    
+    @IBOutlet weak var bottomButton: UIButton!
+    
+    var SignUpMode = true
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func topTapped(_ sender: Any) {
+        if emailTextField.text == "" || passwordTextField.text == "" {
+         displayAlert(title: "Missing Information", message: "You must provide both a email and password")
+            
+        }else{
+            if let email = emailTextField.text{
+                if let password = passwordTextField.text{
+                    
+            //check and see whether they're trying to sign up or to log in
+            if SignUpMode{
+               //Sign UP != not equa
+             
+                Auth.auth().createUser(withEmail: email, password: password, completion: {(User,error) in
+                    if error != nil{
+                        self.displayAlert(title: "Error", message: error!.localizedDescription)
+                    }      else{
+                        print("Sign Up Success!")
+                        self.performSegue(withIdentifier: "driverSegue", sender: nil)
+                        
+                    }          })
+            }else {
+                // Log In
+                Auth.auth().signIn(withEmail: email, password: password, completion: {(User,error) in
+                    if error != nil {
+                        self.displayAlert(title: "Error", message: error!.localizedDescription)
+                    }else{
+                        print("Log In Success")
+                        self.performSegue(withIdentifier: "driverSegue", sender: nil)
+                    }
+                })
+                
+            }
+                }
+            }
+            
+        }
+        
+        
+    }
+    //alert to user & use alert Controller
+    func displayAlert(title: String, message:String ){
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alertController, animated: true, completion: nil)
+        
+        
+    }
+    
+    @IBAction func buttomTapped(_ sender: Any) {
+        if SignUpMode {
+            topButton.setTitle("Log In", for: .normal)
+            bottomButton.setTitle("Switch to Sign up", for: .normal)
+            Riderlabel.isHidden = true
+            drivelabel.isHidden = true
+            driverRiderSwitch.isHidden = true
+            SignUpMode = false
+            
+        }else{
+            topButton.setTitle("Sign UP", for: .normal)
+            bottomButton.setTitle("Switch to Log In", for: .normal)
+            Riderlabel.isHidden = false
+            drivelabel.isHidden = false
+            driverRiderSwitch.isHidden = false
+            SignUpMode = true
+            
+        }
+        
+    }
+    
+}
+
---
+++ Pods/Target Support Files/Pods-CarsPartnerAppTests/Pods-CarsPartnerAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CarsPartnerAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_CarsPartnerAppTests
+@end
